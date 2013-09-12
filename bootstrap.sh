1bddfe033db9651adc2630ed573e40ca23839c4d
Piotr Husiatyński <phusiatynski@gmail.com>
2013-09-12 12:02:46 +0200

    init

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+virtualenv -p /usr/bin/python3 --no-site-packages env
+source env/bin/activate
+pip install -r app/requirements-dev.txt
+cat > overmind/overmind/settings_local.py <<EOF
+DEBUG = True
+ALLOWED_HOSTS = (
+    '127.0.0.1',
+)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'overmind_db.sqlite3'),
+    }
+}
+EOF
+python overmind/manager.py syncdb
---
+++ overmind/overmind/settings.py
@@ -0,0 +1,62 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'h)7ir0!6d0!r2d8+xm2+mpiw7z_#=1yk6==c4avbt@#-g#t4jg'
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'overmind.urls'
+
+WSGI_APPLICATION = 'overmind.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
