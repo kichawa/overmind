9b2c0714822fca145826adb4b404a5f35e357134
Piotr Husiatyński <phusiatynski@gmail.com>
2013-09-14 12:23:36 +0200

    generate tags for imported topics

--- importdb.py
+++ importdb.py
@@ -1,11 +1,22 @@
+import random
 import getpass
-import psycopg2
 import sys
 import sqlite3
+
+import psycopg2
 
 
 
 sqlt_db_path = sys.argv[1]
+
+TAGS = [
+    'howto',
+    'beginner',
+    'xorg',
+    'gtk',
+    'qt',
+    'programming',
+]
 
 
 def copy_users(pg_connection):
@@ -43,11 +54,28 @@ def copy_topics(pg_connection):
 
     sqlt_connection = sqlite3.connect(sqlt_db_path)
     sqlt_c = sqlt_connection.cursor()
+    for tag in TAGS:
+        sqlt_c.execute('''
+            INSERT INTO forum_tag(label)
+            VALUES (?)
+        ''', (tag, ))
     for row in pg_c:
         sqlt_c.execute('''
             INSERT INTO forum_topic(id, author_id, subject, created, updated, response_count)
             VALUES ($1, $2, $3, $4, $4, $5 - 1)
         ''', row)
+        attached_tags = {None}
+        for _ in range(random.randint(1, len(TAGS))):
+            tid = None
+            while tid in attached_tags:
+                tid = random.randint(1, len(TAGS))
+            attached_tags.add(tid)
+
+            sqlt_c.execute('''
+                INSERT INTO forum_topic_tags (topic_id, tag_id )
+                VALUES ($1, $2)
+            ''', (row[0], tid))
+
     pg_c.close()
     sqlt_connection.commit()
 
@@ -74,8 +102,11 @@ def main():
     pg_conn = psycopg2.connect(
             dbname="pg_5908", user="pg_5908u", host='archlinux.megiteam.pl',
             port=5435, password=getpass.getpass('archlinux database password:'))
+    print("Copy users")
     copy_users(pg_conn)
+    print("Copy topics")
     copy_topics(pg_conn)
+    print("Copy posts")
     copy_posts(pg_conn)
     pg_conn.close()
 
