adeb92e1602b34066e9306b839a3f18fbad52665
Piotr Husiatyński <phusiatynski@gmail.com>
2013-09-14 08:42:02 +0200

    redirec / to /forum

--- overmind/overmind/urls.py
+++ overmind/overmind/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', RedirectView.as_view(url='forum/')),
     url(r'^forum/', include('forum.urls', 'forum')),
     url(r'^_/admin/', include(admin.site.urls)),
 )
