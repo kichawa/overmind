538a84245694e55e62356bb1ad91db5c11f86679
Piotr Husiatyński <phusiatynski@gmail.com>
2013-09-24 18:04:38 +0200

    counter GET api resource is all we need

--- overmind/counter/urls.py
+++ overmind/counter/urls.py
@@ -1,16 +1,8 @@
 from django.conf.urls import patterns, url
 
-from counter import views
-
 
 urlpatterns = patterns('counter.views',
     url(r'^/$',
-        views.Main.as_view(),
-        name="main"),
-    url(r'^increment/$',
-        views.Increment.as_view(),
-        name="increment"),
-    url(r'^decrement/$',
-        views.Decrement.as_view(),
-        name="decrement"),
+        'get_counter',
+        name="get"),
 )
--- overmind/counter/views.py
+++ overmind/counter/views.py
@@ -1,50 +1,12 @@
 import json
 
 from django.http import HttpResponse
-from django.views.generic.base import View
 
 from counter import backend
 
 
-
-class CounterView(View):
-    def __init__(self):
-        self.cache = backend.default()
-
-    def json_response(self, request, resp, status=200):
-        return HttpResponse(json.dumps(resp), content_type='application/json',
-                            status=status)
-
-
-class Main(CounterView):
-    def get(self, request):
-        keys = request.REQUEST.getlist('key')
-        resp = self.cache.get(*keys)
-        return self.json_response(request, resp)
-
-    def post(self, request):
-        # we don't support multiple values for one key
-        pairs = dict(request.POST.items())
-        if pairs:
-            resp = self.cache.set(**pairs)
-        return self.json_response(request, resp, 201)
-
-
-class Increment(CounterView):
-    def put(self, request):
-        try:
-            keys = json.loads(request.body.decode('utf8'))
-        except ValueError:
-            return self.json_response(request, {'error': 'Invalid json'}, 400)
-        resp = self.cache.increment(*keys)
-        return self.json_response(request, resp)
-
-
-class Decrement(CounterView):
-    def put(self, request):
-        try:
-            keys = json.loads(request.body.decode('utf8'))
-        except ValueError:
-            return self.json_response(request, {'error': 'Invalid json'}, 400)
-        resp = self.cache.decrement(*keys)
-        return self.json_response(request, resp)
+def get_counter(request):
+    cache = backend.default()
+    resp = cache.get(*request.GET.getlist('key'))
+    return HttpResponse(json.dumps(resp), content_type='application/json',
+                        status=200)
